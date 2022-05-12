59474478ae7041e6f2fb2ec6e0edeff5763dd87f
Stefan Chivu <v-stchivu@microsoft.com>
2022-05-12 23:24:25 +0300

    Added firestore to project and base user model

--- lib/models/base_user.dart
+++ lib/models/base_user.dart
@@ -1,5 +1,50 @@
-class BaseUser{
-  final String uid;
+import 'package:uuid/uuid.dart';
 
-  BaseUser({ required this.uid });
+class BaseUser{
+  String uid = "";
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? phoneNo;
+  String? address;
+  bool? isMeister;
+  String? meisterID;
+
+  BaseUser({ required this.uid});
+  // BaseUser({ required this.uid, 
+  //   required this.email, 
+  //   required this.firstName, 
+  //   required this.lastName,
+  //   required this.phoneNo,
+  //   this.address,
+  //   required this.isMeister,
+  // }) {
+  //   if(isMeister == true){
+  //     var uuid = Uuid();
+
+  //     meisterID = uuid.v1();
+  //   }
+  // }
+
+  Map<String, dynamic> toMap() => {
+    "uid": uid,
+    "email": email,
+    "firstName": firstName,
+    "lastName": lastName,
+    "phoneNo": phoneNo,
+    "address": address,
+    "isMeister": isMeister,
+    "meisterID": meisterID
+  };
+
+  BaseUser.fromMap(Map<String, dynamic> map){
+    uid = map["uid"];
+    email = map["email"];
+    firstName = map["firstName"];
+    lastName = map["lastName"];
+    phoneNo = map["phoneNo"];
+    address = map["address"];
+    isMeister = map["isMeister"];
+    meisterID = map["meisterID"];
+  }
 }
