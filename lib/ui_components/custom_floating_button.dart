31f418a24daf0df88834c76c345f439bb8c1516e
Butoi-Draghici Alina <butoidraghicialina@gmail.com>
2022-05-22 15:21:20 +0300

    MSTR-3: Customized FAB

--- lib/ui_components/custom_floating_button.dart
+++ lib/ui_components/custom_floating_button.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 class CustomFloatingButton extends StatefulWidget {
   final VoidCallback onPressed;
   final IconData icon;
+  final String? heroTag;
   const CustomFloatingButton(
-      {Key? key, required this.onPressed, this.icon = Icons.arrow_forward_ios})
+      {Key? key,
+      required this.onPressed,
+      this.icon = Icons.arrow_forward_ios,
+      this.heroTag})
       : super(key: key);
 
   @override
@@ -15,6 +19,7 @@ class _CustomFloatingButtonState extends State<CustomFloatingButton> {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
+      heroTag: widget.heroTag ?? "FAB",
       onPressed: widget.onPressed,
       child: Icon(widget.icon),
       elevation: 0,
