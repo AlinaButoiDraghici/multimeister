6c43587e9da5601e23ce99f65dfc18c1def76700
Butoi-Draghici Alina <butoidraghicialina@gmail.com>
2022-05-22 15:19:39 +0300

    MSTR-3: Updated meister model and work model

--- lib/models/meister_model.dart
+++ lib/models/meister_model.dart
@@ -4,10 +4,25 @@ import 'package:multimeister/models/work_model.dart';
 class Meister {
   String uid = "";
   String userID = "";
-  List<Work> workList = List.empty();
-  List<Review> reviewList = List.empty();
+  //lists containing work and review uids
+  List<String> workList = List.empty();
+  List<String> reviewList = List.empty();
   Meister({
     required this.uid,
     required this.userID,
   });
+
+  Map<String, dynamic> toMap() => {
+        "uid": uid,
+        "userID": userID,
+        "workList": workList,
+        "reviewList": reviewList,
+      };
+
+  Meister.fromMap(Map<String, dynamic> map) {
+    uid = map["uid"];
+    userID = map["userID"];
+    workList = map["workList"];
+    reviewList = map["reviewList"];
+  }
 }
