ea6b52c8b5310728626d32c0a62000fdc2f3ada4
Butoi-Draghici Alina <butoidraghicialina@gmail.com>
2022-05-27 01:38:10 +0300

    MSTR-3: Added logic to edit profile

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:multimeister/models/base_user.dart';
 import 'package:multimeister/models/hive_user.dart';
+import 'package:multimeister/screens/profile_page.dart';
+import 'package:multimeister/services/database.dart';
 import 'package:multimeister/services/hive.dart';
 import 'package:multimeister/ui_components/custom_button.dart';
 import 'package:multimeister/ui_components/custom_textfield.dart';
@@ -15,7 +18,25 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final _firstNameFormKey = GlobalKey<FormState>();
+  final _lastNameFormKey = GlobalKey<FormState>();
+  final _phoneFormKey = GlobalKey<FormState>();
+  final _cityFormKey = GlobalKey<FormState>();
+  late final TextEditingController _firstNameController;
+  late final TextEditingController _lastNameController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _cityController;
   final HiveUser loggedUser = HiveServices().getUserData()!;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController = TextEditingController(text: loggedUser.firstName);
+    _lastNameController = TextEditingController(text: loggedUser.lastName);
+    _phoneController = TextEditingController(text: loggedUser.phone);
+    _cityController = TextEditingController(text: loggedUser.city);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,26 +79,38 @@ class _EditProfileState extends State<EditProfile> {
               ],
             ),
             SizedBox(height: AppMargins.L),
-            CustomTextField(
-              label: "Prenume",
-              initialValue: loggedUser.firstName,
+            Form(
+              key: _firstNameFormKey,
+              child: CustomTextField(
+                label: "Prenume",
+                controller: _firstNameController,
+              ),
             ),
             SizedBox(height: AppMargins.M),
-            CustomTextField(
-              label: "Nume",
-              initialValue: loggedUser.lastName,
+            Form(
+              key: _lastNameFormKey,
+              child: CustomTextField(
+                label: "Nume",
+                controller: _lastNameController,
+              ),
             ),
             SizedBox(height: AppMargins.M),
-            CustomTextField(
-              label: "Telefon",
-              initialValue: loggedUser.phone,
-              icon: Icons.phone,
+            Form(
+              key: _phoneFormKey,
+              child: CustomTextField(
+                label: "Telefon",
+                icon: Icons.phone,
+                controller: _phoneController,
+              ),
             ),
             SizedBox(height: AppMargins.M),
-            CustomTextField(
-              label: "Localitate",
-              initialValue: loggedUser.city,
-              icon: Icons.location_city,
+            Form(
+              key: _cityFormKey,
+              child: CustomTextField(
+                label: "Localitate",
+                icon: Icons.location_city,
+                controller: _cityController,
+              ),
             ),
             SizedBox(height: AppMargins.M),
             Row(
@@ -91,7 +124,37 @@ class _EditProfileState extends State<EditProfile> {
                   text: "Anuleaza",
                 ),
                 CustomButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    if (_firstNameFormKey.currentState!.validate() &&
+                        _lastNameFormKey.currentState!.validate() &&
+                        _phoneFormKey.currentState!.validate() &&
+                        _cityFormKey.currentState!.validate()) {
+                      BaseUser user = BaseUser(
+                          uid: loggedUser.uid,
+                          email: loggedUser.email,
+                          firstName: _firstNameController.text,
+                          lastName: _lastNameController.text,
+                          phone: _phoneController.text,
+                          isMeister: loggedUser.isMeister,
+                          city: _cityController.text);
+                      DatabaseService databaseService = DatabaseService();
+                      String resultUser = await databaseService.addUser(user);
+                      if (resultUser.contains("success")) {
+                        HiveServices().addUserToBox(user);
+                        Navigator.pop(context);
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage(
+                                    user: HiveServices().getUserData()!,
+                                  )),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text(resultUser)));
+                      }
+                    }
+                  },
                   text: "Editeaza",
                 )
               ],
